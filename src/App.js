153652e7d93d1e76dca946e8dd8fccb0d2cf3eeb
jeffreygohkonnect <93102995+jeffreygohkonnect@users.noreply.github.com>
2021-11-26 08:40:44 +1000

    changed text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactz
+          I was changed
         </a>
       </header>
     </div>
